b6ddb1f77418b169dc89d5e01cc475d550789568
Julio-Tx <julio.jgomesadm@gmail.com>
2022-03-17 11:11:25 +0000

    Comunicação com MongoDB e metodo CREATE incluidos

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,5 +1,7 @@
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
+  res.locals.errors = req.flash('errors');
+  res.locals.success = req.flash('success');
+  res.locals.user = req.session.user;
   next();
 };
 
@@ -8,7 +10,7 @@ exports.outroMiddleware = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if(err && 'EBADCSRFTOKEN' === err.code) {
+  if(err) {
     return res.render('404');
   }
 };
@@ -17,3 +19,12 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user) {
+    req.flash('errors', 'Você precisa fazer login');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+  next();
+}
